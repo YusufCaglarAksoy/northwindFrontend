7f7e4983e62c1d91d09d680d629409bdb77841df
Yusuf Çağlar Aksoy <77413549+YusufCaglarAksoy@users.noreply.github.com>
2021-03-24 19:51:54 +0300

    20.Gün

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,8 +14,12 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded =false;
+  filterText=""
+
   constructor(private productService:ProductService, 
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
     ngOnInit(): void {
       this.activatedRoute.params.subscribe(params=>{
@@ -39,4 +45,14 @@ export class ProductComponent implements OnInit {
     })   
   }
 
-}
+  addToCart(product:Product){
+    if(product.unitsInStock===0){
+      this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+    }
+    else {
+      this.toastrService.success("Sepete Eklendi",product.productName)
+      this.cartService.addToCart(product);
+    }
+  }
+  
+} 
